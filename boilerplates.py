20109c8dd75232cc41f0a2dcf6da23752ba35b1d
bryceandpeas <bryce@bryceandpeas.com>
2020-03-31 11:52:16 +0100

    Add Flask Minimal App Command

---
+++ boilerplates.py
@@ -0,0 +1,15 @@
+import sublime
+import sublime_plugin
+
+class flask_minimal(sublime_plugin.TextCommand):
+	#Generate Code for a minimal Flask application
+
+	def run(self, edit):
+		text  = """
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+"""
+		for region in self.view.sel():
+			self.view.insert(edit, region.begin(), text)
