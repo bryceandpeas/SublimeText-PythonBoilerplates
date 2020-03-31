849442c15d4ce3dba1e76f78ff2d610e8fe33464
bryceandpeas <bryce@bryceandpeas.com>
2020-03-31 12:07:33 +0100

    Add Flask Render Template Command

--- boilerplates.py
+++ boilerplates.py
@@ -16,3 +16,21 @@ def hello_world():
 				self.view.replace(edit, region, text)
 			else:
 				self.view.insert(edit, region.begin(), text)
+
+class flask_render_template(sublime_plugin.TextCommand):
+	#Generate Code for a Flask application that uses render_template()
+
+	def run(self, edit):
+		text  = """
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+"""
+		for region in self.view.sel():
+			if not region.empty():
+				self.view.replace(edit, region, text)
+			else:
+				self.view.insert(edit, region.begin(), text)
