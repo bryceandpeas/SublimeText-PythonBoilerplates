0228a76d464609105ae426c657f2cfb3fbaca12d
bryceandpeas <bryce@bryceandpeas.com>
2020-04-08 01:40:52 +0100

    Add argparse.py convert spaces to tabs

--- boilerplates/arguments/argparse.py
+++ boilerplates/arguments/argparse.py
@@ -2,34 +2,34 @@ import argparse
 
 
 def main(args):
-    return None
+	return None
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser()
 
-    # Required positional argument
-    parser.add_argument("arg", help="Required positional argument")
+	# Required positional argument
+	parser.add_argument("arg", help="Required positional argument")
 
-    # Optional argument flag which defaults to False
-    parser.add_argument("-f", "--flag", action="store_true", default=False)
+	# Optional argument flag which defaults to False
+	parser.add_argument("-f", "--flag", action="store_true", default=False)
 
-    # Optional argument which requires a parameter (eg. -d test)
-    parser.add_argument("-n", "--name", action="store", dest="name")
+	# Optional argument which requires a parameter (eg. -d test)
+	parser.add_argument("-n", "--name", action="store", dest="name")
 
-    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="count",
-        default=0,
-        help="Verbosity (-v, -vv, etc)")
+	# Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+	parser.add_argument(
+		"-v",
+		"--verbose",
+		action="count",
+		default=0,
+		help="Verbosity (-v, -vv, etc)")
 
-    # Specify output of "--version"
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="%(prog)s (version {version})".format(version=__version__))
+	# Specify output of "--version"
+	parser.add_argument(
+		"--version",
+		action="version",
+		version="%(prog)s (version {version})".format(version=__version__))
 
-    args = parser.parse_args()
-    main(args)
+	args = parser.parse_args()
+	main(args)
